8ae19342e0ab979dae0aecc5270a3bb6091e7570
BaraNavaAlan <barahonanavarroalan@gmail.com>
2022-10-12 11:09:43 -0500

    #6 Version 0.5 Completado (funcional)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 require('./bootstrap');
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+import App from './components/App.vue';
+import { routes } from './routes.js';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const app = createApp(App);
+app.use(router);
+app.use(VueAxios, axios);
+app.mount('#app');
